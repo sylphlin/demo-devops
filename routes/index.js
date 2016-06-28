547df1b8c144faa14d6c4d9bdb82b764ff711d92
sylphlin <sylph.lin@gmail.com>
2016-06-28 03:34:16 +0000

    Add Server IP

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var ip = require('ip');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hybrid Cloud Demo', serverIp: ip.address });
 });
 
 module.exports = router;
